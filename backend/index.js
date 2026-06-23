15a3c246534ffbdb8adfa28d1fc52f62275d7ba9
Shogun585 <abhi12344spidey@gmail.com>
2026-06-23 12:26:54 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
 
 const apiRouter = require('./api');
 
 app.use(express.json());
+app.use(cors({
+    origin : "http://localhost:5173"
+}));
 app.use('/api', apiRouter);
 
 app.listen(PORT, ()=>{
