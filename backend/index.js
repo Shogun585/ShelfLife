adb0b61c17cb78476cb5e349911c773ffd86531a
Shogun585 <abhi12344spidey@gmail.com>
2026-06-23 12:33:41 +0530

    delegated cors options to env files

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT;
+const ORIGIN = process.env.ORIGIN;
 const app = express();
 
 const apiRouter = require('./api');
 
 app.use(express.json());
 app.use(cors({
-    origin : "http://localhost:5173"
+    origin : ORIGIN
 }));
 app.use('/api', apiRouter);
 
