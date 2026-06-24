431aab0cad2005d33c339bb4cae61f036fee4a00
Shogun585 <abhi12344spidey@gmail.com>
2026-06-24 09:42:09 +0530

    add self ping to avoid cold start

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,16 @@ const app = express();
 
 const apiRouter = require('./api');
 
+app.get('/keep-alive', (req, res)=>{
+    res.send("Keep alive");
+});
+
+setInterval(()=>{
+    fetch(`${req.protocol}://${req.get('host')}/keep-alive`)
+        .then(()=>console.log("Pinged self to stay alive."))
+        .catch((err)=>console.error("Ping failed : ", err.message));
+}, 5 * 60 *1000);
+
 app.use(express.json());
 app.use(cors({
     origin : ORIGIN
