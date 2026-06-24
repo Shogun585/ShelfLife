128e82a966c829bcb97eb63b0ba572f5316ef0b8
Shogun585 <abhi12344spidey@gmail.com>
2026-06-24 10:07:46 +0530

    fixed url for self ping

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT;
 const ORIGIN = process.env.ORIGIN;
+const BASE_URL = process.env.BASE_URL;
 const app = express();
 
 const apiRouter = require('./api');
@@ -12,7 +13,7 @@ app.get('/keep-alive', (req, res)=>{
 });
 
 setInterval(()=>{
-    fetch(`${req.protocol}://${req.get('host')}/keep-alive`)
+    fetch(`${BASE_URL}/keep-alive`)
         .then(()=>console.log("Pinged self to stay alive."))
         .catch((err)=>console.error("Ping failed : ", err.message));
 }, 5 * 60 *1000);
