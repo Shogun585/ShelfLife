288b86bd3f349f025791ec9df0da81690b2219ab
Shogun585 <abhi12344spidey@gmail.com>
2026-06-27 02:57:30 +0530

    added ccontext provider for tanstack  query; created and added a page loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,20 @@ import Neighborhood from "./components/neighbourhood/Neighbourhood";
 import HouseholdSetup from "./pages/HouseholdSetup";
 import { ProtectedRoute } from "./components/auth/ProtectedRoute"; // Import the bouncer
 import React, { Suspense, useEffect } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const KitchenPage = React.lazy(() => import('./pages/KitchenPage'))
 
+const queryClient = new QueryClient();
+
 export default function App() {
+
+  const PageLoader = () => (
+    <div className="flex h-screen w-full items-center justify-center bg-sky-200">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-amber-900 border-t-transparent"></div>
+    </div>
+  );
 
   useEffect(() => {
     console.log(
@@ -17,42 +27,45 @@ export default function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+        <Routes>
 
-        <Route path="/" element={<AuthPage />} />
-        
-        <Route 
-          path="/setup" 
-          element={
-            <ProtectedRoute>
-              <HouseholdSetup />
-            </ProtectedRoute>
-          } 
-        />
-        
-        <Route 
-          path="/neighborhood" 
-          element={
-            <ProtectedRoute>
-              <Neighborhood />
-            </ProtectedRoute>
-          } 
-        />
-        
-        <Route 
-          path="/kitchen" 
-          element={
-            <ProtectedRoute>
-              <Suspense fallback={null}>
-                <KitchenPage />
-              </Suspense>
-            </ProtectedRoute>
-          } 
-        />
-        
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/" element={<AuthPage />} />
+          
+          <Route 
+            path="/setup" 
+            element={
+              <ProtectedRoute>
+                <HouseholdSetup />
+              </ProtectedRoute>
+            } 
+          />
+          
+          <Route 
+            path="/neighborhood" 
+            element={
+              <ProtectedRoute>
+                <Neighborhood />
+              </ProtectedRoute>
+            } 
+          />
+          
+          <Route 
+            path="/kitchen" 
+            element={
+              <ProtectedRoute>
+                <Suspense fallback={<PageLoader />}>
+                  <KitchenPage />
+                </Suspense>
+              </ProtectedRoute>
+            } 
+          />
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+    
   );
 }
